8a0e4e85b4b4c18423b678fb329a8765a42331d5
AbraChan <1861389915@163.com>
2023-07-22 14:20:04 +0800

    Create cDemo folder

---
+++ C_Cpp/cDemo/3.3_Integer.c
@@ -0,0 +1,16 @@
+//#include <stdio.h>
+//
+//int main()
+//{
+//	short a = 10;
+//	int b = 100;
+//
+//	int short_length = sizeof a;
+//	int int_length = sizeof(b);
+//	int long_length = sizeof(long);
+//	int char_length = sizeof(char);
+//
+//	printf("short=%d\nint=%d\nlong=%d\nchar=%d\n", short_length, int_length, long_length, char_length);
+//
+//	return 0;
+//}
---
+++ C_Cpp/cDemo/9.8_pointer_as_func_return.c
@@ -0,0 +1,37 @@
+//#include <stdio.h>
+//#include<string.h>
+
+//char *strlong(char *str1, char *str2){
+//	//形参 str1 和 str2 可能是一个字符类型的指针
+//	//也可能是一个字符串指针，我们这里是作为字符串指针来使用的
+//	if (strlen(str1) >= strlen(str2)){
+//		return str1;
+//	}
+//	else{ 
+//		return str2;
+//	}
+//}
+//
+//int main() {
+//	char str1[30], str2[30], *str;
+//	gets(str1);
+//	gets(str2);
+//
+//	str = strlong(str1, str2);
+//	printf("Longer string: %s\n", str);
+//
+//	return 0;
+//}
+
+//int *func() {
+//	int n = 100;
+//	return &n;
+//}
+//
+//int main() {
+//	int *p = func(), n;
+//	printf("c.biancheng.net\n");
+//	n = *p;
+//	printf("value=%d\n", n);
+//	return 0;
+//}
